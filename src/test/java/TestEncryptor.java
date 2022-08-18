6e25235d34aa5550a43a3f758ff504a8e1f73e1c
exadmin <exadmin@mail.ru>
2022-08-18 14:53:09 +0300

    Version 1.2 Release Candidate

--- src/test/java/TestEncryptor.java
+++ src/test/java/TestEncryptor.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestEncryptor {
-
     @Test
     public void justEncryptTest() {
         String sourceText = "Hello World!";
